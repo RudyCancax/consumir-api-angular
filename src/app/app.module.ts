b8515fa6213f5b0935d36662f354f2b5eacbd478
RudyCancax <rudycancax@gmail.com>
2020-03-20 15:48:51 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Importamos el módulo HttpClient, ademmás lo agregamos a imports
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+// Importamos el servicio 'usuario' que creamos
+import { UsuarioService } from './usuario.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UsuarioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/usuario.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(protected http: HttpClient) { }
+
+
+  getUsuarios() {
+    return this.http.get('https://randomuser.me/api/?results=25');
+  }
+}
+
+
